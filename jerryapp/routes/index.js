d55b3ebe453a9af60db91408f8c5e005a7ef12c1
i042416 <cle.ee@163.com>
2017-12-09 16:46:05 +0800

    cross domain enabled

--- jerryapp/routes/index.js
+++ jerryapp/routes/index.js
@@ -2,6 +2,15 @@ var request = require('request');
 
 module.exports = function (app) {
 
+  app.all('*', function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+      res.header("X-Powered-By",' 3.2.1');
+      res.header("Content-Type", "application/json;charset=utf-8");
+      next();
+    });
+  
   app.route('/').get(function (req, res) {
         var url = "https://www.baidu.com";
         console.log('/:' + url);
