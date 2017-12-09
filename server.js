49d0977797e096f7e48a10f8448035e0b2a24c73
i042416 <cle.ee@163.com>
2017-12-09 16:35:40 +0800

    add delta part: baidu

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 //    routesEngine = require('./jerryapp/routes/index.js'); // index.js actually
     // X, X.js, X.json and X.node see blog http://www.ruanyifeng.com/blog/2015/05/require.html
-// var request = require('request');
+var request = require('request');
     
 var app = express();
 
@@ -33,5 +33,19 @@ app.listen(process.env.PORT || 3000, function () {
 
 app.get('/', function(req, res){
       console.log("method: " + req.method);
-    res.send("Hello World");
+      var url = "https://www.baidu.com";
+      console.log('/:' + url);
+      // Jerry 2017-12-9 11:04AM - no proxy works under normal network environment
+      var options = {
+            url: url,
+            method: "GET"
+      };
+      request(options,function(error,response,data){
+        if(data){
+            res.send(data);
+        }else {
+            res.send("Jerry error: " + error);
+            console.log(error);
+        }
+          });
   });
