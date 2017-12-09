9c6d67ef0e05565e636fd12507efc15d150dc44f
i042416 <cle.ee@163.com>
2017-12-09 15:53:48 +0800

    still does not work

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var port = 18080;
 var express = require('express'),
     routesEngine = require('./jerryapp/routes/index.js'); // index.js actually
     // X, X.js, X.json and X.node see blog http://www.ruanyifeng.com/blog/2015/05/require.html
-
+var request = require('request');
     
 var session = require("./jerryapp/session/session.js"),
     cookieParser = require('cookie-parser'),
@@ -43,10 +43,33 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 
 
-    routesEngine(app);
+    // routesEngine(app);
 
 
     app.listen(port, function () {
       // c:\\code\\git\\wechat\\app
         console.log('Listening on port 18080, process.cwd(): ' + process.cwd() + " port: " + port );
     });
+
+    app.route('/').get(function (req, res) {
+        var url = "https://www.baidu.com";
+        console.log('/:' + url);
+        // res.send("Hello");
+        // Jerry 2017-12-9 11:04AM - no proxy works under normal network environment
+          var options = {
+            url: url,
+            /*
+            host:'proxy.sha.sap.corp',
+            port:'8080', */
+            method: "GET"
+          };
+          request(options,function(error,response,data){
+            if(data){
+              res.send(data);
+            }else {
+              res.send("Jerry error: " + error);
+              console.log(error);
+            }
+          });
+        });
+
