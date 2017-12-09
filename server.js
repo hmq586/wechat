748e2331a355458a4578fe6910dabf46def2d5f9
i042416 <cle.ee@163.com>
2017-12-09 16:28:09 +0800

    minify

--- server.js
+++ server.js
@@ -1,55 +1,15 @@
 var port = 18080;
-var express = require('express'),
+var express = require('express');
     routesEngine = require('./jerryapp/routes/index.js'); // index.js actually
     // X, X.js, X.json and X.node see blog http://www.ruanyifeng.com/blog/2015/05/require.html
 var request = require('request');
     
-var session = require("./jerryapp/session/session.js"),
-    cookieParser = require('cookie-parser'),
-    babelCore = require('babel-core'),
-    bodyParser = require('body-parser');
-
-
 var app = express();
 
-  
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-
-// express.static: This is a built-in middleware function in Express. 
-// It serves static files and is based on serve-static.
-    
-
-app.use('/public', express.static(process.cwd() + '/public'));
-    app.use('/client', express.static(process.cwd() + '/client'));
-    // mount sub application
-    app.use('/controllers', express.static(process.cwd() + '/jerryapp/controllers'));
-
-
-
-  // 该方法用于在所有请求方法的路径中装入中间件函数。
-
-    app.all('*', function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "X-Requested-With");
-      res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-      res.header("X-Powered-By",' 3.2.1');
-      res.header("Content-Type", "application/json;charset=utf-8");
-      next(); // pass control to the next handler
-    });
-
-
-
-    // routesEngine(app);
-
-
-    app.listen(port, function () {
+app.listen(port, function () {
       // c:\\code\\git\\wechat\\app
-        console.log('Listening on port 18080, process.cwd(): ' + process.cwd() + " port: " + port );
-    });
+  console.log('Listening on port 18080, process.cwd(): ' + process.cwd() + " port: " + port );
+});
 
     /*
     app.route('/').get(function (req, res) {
@@ -72,7 +32,7 @@ app.use('/public', express.static(process.cwd() + '/public'));
         });
     */
 
-    app.get('/', function(req, res){
+app.get('/', function(req, res){
       console.log("method: " + req.method);
     res.send("Hello World");
   });
