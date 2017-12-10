b2e2608eb7e578ad7fbd8bf75bf6d7e0ceec4dcc
i042416 <cle.ee@163.com>
2017-12-10 11:42:38 +0800

    add a trace

--- jerryapp/routes/index.js
+++ jerryapp/routes/index.js
@@ -44,6 +44,7 @@ module.exports = function (app) {
         var MsgType = getXMLNodeValue('MsgType',_da);
         var Content = getXMLNodeValue('Content',_da);
         Content = "**** Add by Jerry " + Content + " ****"; 
+        console.log("new Content: " + Content);
         var MsgId = getXMLNodeValue('MsgId',_da);
         var xml = '<xml><ToUserName>'+FromUserName+'</ToUserName><FromUserName>'+ToUserName+'</FromUserName><CreateTime>'+CreateTime+'</CreateTime><MsgType>'+MsgType+'</MsgType><Content>'+Content+'</Content></xml>';
         res.send(xml);
