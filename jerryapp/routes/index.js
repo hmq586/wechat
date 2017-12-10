f9c5fb99f30439a49567b486ce24aa790e2c07d8
i042416 <cle.ee@163.com>
2017-12-10 11:12:38 +0800

    extract wechat token verification

--- jerryapp/routes/index.js
+++ jerryapp/routes/index.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var jsSHA = require('jssha');
+var tokenValidationTool = require("../tool/tokenValidation.js");
 
 module.exports = function (app) {
 
@@ -13,31 +14,7 @@ module.exports = function (app) {
     });
 
   app.route('/').get(function(req,res){
-    var token="jerry";
-    var signature = req.query.signature,
-      timestamp = req.query.timestamp,
-      echostr   = req.query.echostr,
-      nonce     = req.query.nonce;
-
-      console.log("signature: " + signature);
-      console.log("timestamp: " + timestamp);
-      console.log("echostr: " + echostr);
-      oriArray = new Array();
-      oriArray[0] = nonce;
-      oriArray[1] = timestamp;
-      oriArray[2] = token;
-      oriArray.sort();
-      var original = oriArray.join('');
-
-      var shaObj = new jsSHA("SHA-1", 'TEXT');
-      shaObj.update(original);
-      var scyptoString = shaObj.getHash('HEX');
-      console.log("calculated string: " + scyptoString);
-     if (signature == scyptoString) {
-        res.send(echostr);
-     } else {
-        res.send('bad token');
-     }
+    tokenValidationTool(req, res);
   });
 
   app.route('/test').get(function (req, res) {
