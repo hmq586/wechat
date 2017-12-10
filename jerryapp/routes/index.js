f55608c79697b0ba4bc9746c71b62caa82587218
i042416 <cle.ee@163.com>
2017-12-10 11:37:18 +0800

    test on post

--- jerryapp/routes/index.js
+++ jerryapp/routes/index.js
@@ -4,6 +4,12 @@ var tokenValidationTool = require("../tool/tokenValidation.js");
 
 module.exports = function (app) {
 
+  function getXMLNodeValue(node_name,xml){
+    var tmp = xml.split("<"+node_name+">");
+    var _tmp = tmp[1].split("</"+node_name+">");
+    return _tmp[0];
+  }
+
   app.all('*', function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -18,12 +24,29 @@ module.exports = function (app) {
       tokenValidationTool(req, res);
     }
     else{
-      console.log("game on");
-      for( var a in req){
-        console.log("name: " + a + " value: " + req[a]);
-      }
-      res.send("Welcome");
+      res.send("Hello World");
     }
+  });
+
+  app.route('/').post(function(req,res){
+
+    var _da;
+    req.on("data",function(data){
+      /*微信服务器传过来的是xml格式的，是buffer类型，因为js本身只有字符串数据类型，
+      所以需要通过toString把xml转换为字符串*/
+        _da = data.toString("utf-8");
+    });
+
+    req.on("end",function(){
+        var ToUserName = getXMLNodeValue('ToUserName',_da);
+        var FromUserName = getXMLNodeValue('FromUserName',_da);
+        var CreateTime = getXMLNodeValue('CreateTime',_da);
+        var MsgType = getXMLNodeValue('MsgType',_da);
+        var Content = getXMLNodeValue('Content',_da);
+        var MsgId = getXMLNodeValue('MsgId',_da);
+        var xml = '<xml><ToUserName>'+FromUserName+'</ToUserName><FromUserName>'+ToUserName+'</FromUserName><CreateTime>'+CreateTime+'</CreateTime><MsgType>'+MsgType+'</MsgType><Content>'+Content+'</Content></xml>';
+        res.send(xml);
+    });
   });
 
   app.route('/test').get(function (req, res) {
