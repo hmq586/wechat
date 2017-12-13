8a41c783809b6bd5d5eccb833b7302e478d213c6
i042416 <cle.ee@163.com>
2017-12-13 11:42:07 +0800

    could not receive text sent from Wechat API

--- jerryapp/service/postMessageToUser.js
+++ jerryapp/service/postMessageToUser.js
@@ -2,6 +2,7 @@ var config = require("../../config.js");
 var request = require("request");
 
 function sendWCMeaasge(toUser,sMessage){
+	console.log("begin to send message to user: " + toUser + " with message: " + sMessage);
     var options = {
             url:"https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=" +
             config.access_token, 
@@ -18,7 +19,11 @@ function sendWCMeaasge(toUser,sMessage){
               }
                 }
           };
-      request(options,function(error,response,data){});
+      request(options,function(error,response,data){
+      	console.log("error? " + error);
+        console.log("response: " + response);
+        console.log("data: " + data);
+      });
   }
 
 module.exports = sendWCMeaasge;
