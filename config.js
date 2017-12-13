b8dde45a42514ac6334ec723c88b2716513ff432
i042416 <cle.ee@163.com>
2017-12-13 14:02:38 +0800

    token not valid

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ var config = {
    individualCustomerurl: "https://my500203.c4c.saphybriscloud.cn/sap/c4c/odata/v1/c4codata/IndividualCustomerCollection/",
    credential: "wangjerry37:Saptest12",
    testAccount: "o0KlM1i2_4-zHRmDk-IWGRlA1Cjc",
-   accessToken: "4_9WhJ0S8AXsR8uz1BGgjFV00mRSiB8mnIPnkoH22xg5eu8QJSis-YLMW-PaakZpt9521PsGlytYUf8uvtWMUPQ1L9l_QF8zGU_MTHe_Ply5v-7oqTX1-5ISDSbqCUEqv2gzJkyaiaQTVUVgmEUGAfAEABNH"
+   accessToken: "4_Kxv-HMtXK1375uwwkOfMfh-KvtkEO4blgZe47z5Dh7sIRgqT9zi0gO22c6USJZel9TaX9jbNYB5cKfsZyoAprIx89laeoS9hLwOEUhhjg5OujcYwElSEnKUi-psPQMbAHADEA"
 };
 module.exports = config;
---
+++ unittest/sendmessage.js
@@ -0,0 +1,43 @@
+var config = require("../config.js");
+var request = require('request');
+
+function printObject(oData){
+	for( var a in oData){
+		console.log("key: " + a);
+		console.log("value: " + oData[a]);
+		if( typeof oData[a] === "object"){
+			printObject(oData[a]);
+		}
+	}
+}
+
+
+function sendWCMeaasge(toUser,sMessage){
+	console.log("begin to send message to user: " + toUser + " with message: " + sMessage);
+    var options = {
+            url:"https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=" +
+            config.access_token, 
+            method: "POST",
+            json:true,
+            headers: {
+            	"content-type": "application/json"},
+            body:{
+              "touser":toUser,
+              "msgtype":"text",
+              "text":
+              {
+                   "content":sMessage
+              }
+                }
+          };
+      request(options,function(error,response,data){
+      	console.log("error? " + error);
+        console.log("response: " + response);
+        console.log("data: " + data);
+        debugger;
+        console.log("Status message: " + response.statusMessage);
+        console.log("Data: " + data.errmsg);
+      });
+  }
+
+  sendWCMeaasge(config.testAccount, "Jerry22");
