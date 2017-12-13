41b61299434a21c56f82f9072179be901f5afbd1
i042416 <cle.ee@163.com>
2017-12-13 11:47:25 +0800

    add trace

--- jerryapp/service/postMessageToUser.js
+++ jerryapp/service/postMessageToUser.js
@@ -1,6 +1,16 @@
 var config = require("../../config.js");
 var request = require("request");
 
+function printObject(oData){
+	for( var a in oData){
+		console.log("key: " + a);
+		console.log("value: " + oData[a]);
+		if( typeof oData[a] === "object"){
+			printObject(oData[a]);
+		}
+	}
+}
+
 function sendWCMeaasge(toUser,sMessage){
 	console.log("begin to send message to user: " + toUser + " with message: " + sMessage);
     var options = {
@@ -23,6 +33,10 @@ function sendWCMeaasge(toUser,sMessage){
       	console.log("error? " + error);
         console.log("response: " + response);
         console.log("data: " + data);
+        console.log("response...............................................");
+        printObject(response);
+        console.log("data.....................................................");
+        printObject(data);
       });
   }
 
