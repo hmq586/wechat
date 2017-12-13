525ec992a805df94ded218ee6dc084500266f112
i042416 <cle.ee@163.com>
2017-12-13 11:24:29 +0800

    add test account id

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var config = {
    individualCustomerurl: "https://my500203.c4c.saphybriscloud.cn/sap/c4c/odata/v1/c4codata/IndividualCustomerCollection/",
-   credential: "wangjerry37:Saptest12"
+   credential: "wangjerry37:Saptest12",
+   testAccount: "o0KlM1i2_4-zHRmDk-IWGRlA1Cjc"
 };
 module.exports = config;
