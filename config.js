b1a32eb6ef0a5e5e6cd8ebb1f36c7684f39c1741
i042416 <cle.ee@163.com>
2017-12-19 10:52:17 +0800

    add test account appid:

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ var config = {
    indivudualCustomerNewurl: "https://my500203.c4c.saphybriscloud.cn/sap/c4c/odata/cust/v1/zindividualcustomer/CustomerCommonCollection?$filter=",
    credential: "wangjerry37:NewUser1A",
    testAccount: "o0KlM1i2_4-zHRmDk-IWGRlA1Cjc",
+   testAccountAppid: "wx73b49bfe02fd3a17",
+   testAccountSecret: "8a269a9916c32069901c2e6b6f3f16a6",
    accessToken: "4_F72OagzKUXulTQV68gt_dEDeBMrA0M55jVCZMUqZmd6s6fWvWX80gA1lb_-pLPi0-AydwiI_YdAz_owFqO-ygp6QxgTTON5wQTo1yHFgPZA7QipYHVPSlwEVCJgGFFdAIAWVV"
 };
 module.exports = config;
