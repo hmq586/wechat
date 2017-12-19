f6bcb216af548fa2f8c32570717a45da87c43366
i042416 <cle.ee@163.com>
2017-12-19 10:56:43 +0800

    at least url fine

--- unittest/oAuth.js
+++ unittest/oAuth.js
@@ -1,3 +1,44 @@
-console.log("1");
-debugger;
-console.log("2");
+var config = require("../config.js");
+var request = require('request');
+
+var code = "021ko0iC0qyaeg2n5RhC02iIhC0ko0iT";
+var appid = "";
+var secret = "";
+
+// get access token
+var url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" + 
+config.testAccountAppid + "&secret=" + config.testAccountSecret + "&code=" + code + "&grant_type=authorization_code";
+console.log("url sent: " + url);
+
+var getTokenOptions = {
+        url: code,
+        method: "GET",
+        json:true,
+        headers: {
+            "content-type": "application/json"
+        }
+};
+
+function printObject(oData){
+  for( var a in oData){
+    console.log("key: " + a);
+    console.log("value: " + oData[a]);
+    if( typeof oData[a] === "object"){
+      printObject(oData[a]);
+    }
+  }
+}
+
+function getToken() {
+  return new Promise(function(resolve,reject){
+      var requestC = request.defaults({jar: true});
+      requestC(getTokenOptions,function(error,response,body){
+       debugger;
+
+       printObject(body.d.results[0]);
+
+      }); // end of requestC
+     });
+} 
+
+getToken();
