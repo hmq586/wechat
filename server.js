a1aedb1077b2b6af769c13f8c8c2cee638d0e5b9
i042416 <cle.ee@163.com>
2017-12-20 10:43:07 +0800

    first redis try

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ var app = express();
 app.use('/ui5', express.static(process.cwd() + '/webapp'));
 routesEngine(app);
 
+var redis = require("redis"),
+    client = redis.createClient();
+
+client.set("some key", "i042416");
+client.get("some key", function(err, reply) {
+    // reply is null when the key is missing
+    console.log("Jerry Redis practice: ******************** " + reply);
+});
+
 app.listen(process.env.PORT || 3000, function () {
       // c:\\code\\git\\wechat\\app
   console.log('Listening on port, process.cwd(): ' + process.cwd() );
