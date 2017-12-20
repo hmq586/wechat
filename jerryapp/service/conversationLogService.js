3863be7084524912243e27eeda5d0b452f3aad16
i042416 <cle.ee@163.com>
2017-12-20 16:22:38 +0800

    add sendtime

--- jerryapp/service/conversationLogService.js
+++ jerryapp/service/conversationLogService.js
@@ -3,18 +3,28 @@ var formattedValue = require("../tool/formatValue.js");
 var redisClient = require("./redisClient.js");
 var config = require("../../config.js");
 
+Date.prototype.today = function () { 
+    return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+}
+
+Date.prototype.timeNow = function () {
+     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+}
+
 function logConversation(wholeContent, question, answer){
 	var fromUserId = formattedValue(getXMLNodeValue('FromUserName', wholeContent));
 	var toUserId = formattedValue(getXMLNodeValue('ToUserName', wholeContent));
 	var fromUserName = config.userMap[fromUserId] || fromUserId;
 	var toUserName = config.userMap[toUserId] || toUserId;
-	redisClient.insert(toUserId, objectToString(fromUserName, toUserName, question, answer));
+	var datetime = "Send Time: " + new Date().today() + "  " + new Date().timeNow();
+	redisClient.insert(toUserId, objectToString(fromUserName, toUserName, datetime, question, answer));
 };
 
-function objectToString(fromUserName, toUserName, question, answer){
+function objectToString(fromUserName, toUserName, datetime, question, answer){
 	var record = {
 		"from": fromUserName,
 		"to": toUserName,
+		"sendTime": datetime,
 		"question": question,
 		"answer": answer
 	};
