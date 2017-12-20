387f2559b5619ba548bbbfc144442b3156c0b061
i042416 <cle.ee@163.com>
2017-12-20 11:19:32 +0800

    redis local test ok

---
+++ unittest/redisLocalTest.js
@@ -0,0 +1,12 @@
+var redis = require("redis"),
+    client = redis.createClient("redis://h:p99a8dd0d92871b9ffe7a026e6d70beecd7f2a0e743fa1e2840a58ce048f41c4a@ec2-34-237-158-248.compute-1.amazonaws.com:9479"); // by default localhost will be used!!
+
+client.on("error", function (err) {
+    console.log("Redis failed! Error " + err);
+});
+
+client.set("some key", "i042416");
+client.get("some key", function(err, reply) {
+    // reply is null when the key is missing
+    console.log("Jerry Redis practice: ******************** " + reply);
+});
