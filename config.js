0d2606cd847a5f221d6b52a2a2746ff163f82f4e
hmq586 <39184763+hmq586@users.noreply.github.com>
2018-08-31 16:09:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ var config = {
    credential_qxl: "_QXL:Saptest1",
    credential_odata: "WANGJERRY62818:Saptest1",
    testAccount: "o0KlM1i2_4-zHRmDk-IWGRlA1Cjc",
-   testAccountAppid: "wx73b49bfe02fd3a17",
-   testAccountSecret: "8a269a9916c32069901c2e6b6f3f16a6",
-   accessToken: "11_PcK8E4Rf1r9dUrJniF7FQnxzwpDh7yzMfP4jdS0jXuup4emKmHEg3VSJp48DxmFX3NAeDLcFa7iuC-whpMGkBIVmQQ_1funciX0pqePavltWNqYCv7Rpj2YCwVyMjqBMJOIXbLN_ZU8HK_HQVJSbAIAQEN",
+   testAccountAppid: "wx27da4d9c25135f8b",
+   testAccountSecret: "b8a6ac1939347ca5fddc4ebeca204710",
+   accessToken: "wechat123456",
    userMap: {
    	"gh_106591ffb8a3": "Jerry test Subscription Account",
    	"o0KlM1i2_4-zHRmDk-IWGRlA1Cjc": "null"
